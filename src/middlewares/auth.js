2aa1e1f1e084430b637885a93603471d27619a06
MaxiAlem <maerc.maxi@gmail.com>
2025-11-24 00:04:47 -0300

    cambios menores en readme

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,33 +1,43 @@
-
-export const authMiddleware = (options = { redirect: false }) => {
+export const authMiddleware = () => {
   return (req, res, next) => {
     if (!req.session.userId) {
-      if (options.redirect) {
+      
+      // reuquin para detectar si es navegador (HTML) o API (JSON)
+      const aceptaHTML = req.headers.accept?.includes("text/html");
+
+      if (aceptaHTML) {
         return res.redirect("/auth/login"); // para vistas
       }
+
       return res.status(401).json({ mensaje: "No autorizado" }); // para API
     }
+
     res.locals.usuario = {
       id: req.session.userId,
       rol: req.session.rol,
       nombre: req.session.nombre || "Usuario",
     };
+
     next();
   };
 };
 
-export const rolMiddleware = (rolesPermitidos, options = { redirect: false }) => {
+
+export const rolMiddleware = (rolesPermitidos) => {
   return (req, res, next) => {
     const userRol = req.session.rol;
+
     if (!userRol || !rolesPermitidos.includes(userRol)) {
-      if (options.redirect) {
+
+      const aceptaHTML = req.headers.accept?.includes("text/html");
+  //  Si es navegador redirigir
+      if (aceptaHTML) {
         return res.status(403).render("error", { message: "Acceso denegado" });
       }
+  //  Si es api respuesta
       return res.status(403).json({ mensaje: "Acceso denegado" });
     }
+
     next();
   };
 };
-
-
-//seteado para funcionar en apis y en viiews de pug
